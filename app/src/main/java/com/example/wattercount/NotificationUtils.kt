e547d8a8ee0b4f15e221adec96c7f3152f4839c0
Roman <hutsonr155@gmail.com>
2023-07-15 19:33:36 +0300

    change app icon

--- app/src/main/java/com/example/wattercount/NotificationUtils.kt
+++ app/src/main/java/com/example/wattercount/NotificationUtils.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import java.util.*
@@ -19,64 +20,62 @@ class NotificationUtils(private val context: Context) {
     private val notificationId = 1
     private val alarmRequestCode = 100
 
-
-    fun scheduleNotification() {
-        createNotificationChannel()
-
-        val intent = Intent(context, NotificationReceiver::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            context,
-            alarmRequestCode,
-            intent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.HOUR_OF_DAY, 1)
-
-        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        alarmManager.setInexactRepeating(
-            AlarmManager.RTC_WAKEUP,
-            calendar.timeInMillis,
-            AlarmManager.INTERVAL_HOUR * 1,
-            pendingIntent
-        )
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                channelName,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-
-            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
+//
+//    fun scheduleNotification() {
+//        createNotificationChannel()
+//
+//        val intent = Intent(context, NotificationReceiver::class.java)
+//        val pendingIntent = PendingIntent.getBroadcast(
+//            context,
+//            alarmRequestCode,
+//            intent,
+//            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+//        )
+//
+//        val calendar = Calendar.getInstance()
+//        calendar.add(Calendar.HOUR_OF_DAY, 1)
+//
+//        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//
+//        alarmManager.setInexactRepeating(
+//            AlarmManager.RTC_WAKEUP,
+//            calendar.timeInMillis,
+//            AlarmManager.INTERVAL_HOUR * 1,
+//            pendingIntent
+//        )
+//    }
+//
+//    private fun createNotificationChannel() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val channel = NotificationChannel(
+//                channelId,
+//                channelName,
+//                NotificationManager.IMPORTANCE_DEFAULT
+//            )
+//
+//            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.createNotificationChannel(channel)
+//
+//            val currChannel = notificationManager.getNotificationChannel(channelId)
+//            if (currChannel == null) {
+//                notificationManager.createNotificationChannel(channel);
+//                Toast.makeText(context, "channel created", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(context, "channel exists", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 
     fun showNotification() {
         val notificationBuilder = NotificationCompat.Builder(context, channelId)
-            .setSmallIcon(R.drawable.ic_circle_notification)
+            .setSmallIcon(R.drawable.ic_raindrop)
             .setContentTitle("Напоминание о питье воды")
-            .setContentText("Пора пить воду!")
+            .setContentText("Пора выпить воды")
             .setColor(ContextCompat.getColor(context, R.color.primary))
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
-            !notificationManager.isNotificationPolicyAccessGranted
-        ) {
-            // Запросите разрешение у пользователя для отправки уведомлений
-            val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
-            context.startActivity(intent)
-        } else {
-            // Отправка уведомления
-            notificationManager.notify(notificationId, notificationBuilder.build())
-        }
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 }
