a0e39e4854c384cfd199942b84ee0a5cad9506d8
Roman <hutsonr155@gmail.com>
2023-07-15 19:34:41 +0300

    upd notifi

--- app/src/main/java/com/example/wattercount/MainActivity.kt
+++ app/src/main/java/com/example/wattercount/MainActivity.kt
@@ -1,26 +1,38 @@
 package com.example.wattercount
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.wattercount.Fragments.HomeFragment
-import com.example.wattercount.Fragments.ProfileFragment
 import com.example.wattercount.Fragments.StatisticFragment
 import com.example.wattercount.databinding.ActivityMainBinding
 
-
+private const val INTERVAL_THREE_HOUR = 10800000L // 10800000L
 class MainActivity : AppCompatActivity() {
     private val TAG = "debugTag"
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var notificationUtils: NotificationUtils
+    private var alarmManager: AlarmManager? = null
+    private lateinit var alarmIntent: PendingIntent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        notificationUtils = NotificationUtils(this)
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        alarmIntent = Intent(this, NotificationReceiver::class.java).let { intent ->
+            PendingIntent.getBroadcast(applicationContext, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        }
+        alarmManager?.setInexactRepeating(
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            SystemClock.elapsedRealtime() + INTERVAL_THREE_HOUR,
+            INTERVAL_THREE_HOUR,
+            alarmIntent
+        )
 
         setContentView(binding.root)
         replaceFragment(HomeFragment())
